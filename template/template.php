d3d602e7cd7c4b75089fba6a3c7862a18afc3e59
tvn24van <wojciech.smaza@gmail.com>
2023-02-15 01:18:36 +0100

    footer separation

--- template/template.php
+++ template/template.php
@@ -20,39 +20,7 @@
     </main>
 
     <footer class="window">
-        <div class="title-bar">
-            <div class="title-bar-text">
-                &copy; <script>document.write(new Date().getFullYear())</script> <?php echo(config('name').' v'.config('version')) ?>
-            </div>
-            <div class="title-bar-controls">
-                <button aria-label="Minimize"></button>
-                <button aria-label="Maximize"></button>
-                <button aria-label="Close"></button>
-            </div>
-        </div>
-        <div class="window-body footer-window-body">
-            <ul class="tree-view">
-                <li>
-                    W projekt zaangażowani byli następujący kontrybutorzy:
-                    <ul>
-                        <li><a target="_blank" href="https://github.com/tvn24van"><img src="https://github.com/tvn24van.png">Wojciech Smaza s26596</a></li>
-                        <li><a target="_blank" href="https://github.com/zahutech"><img src="https://github.com/zahutech.png">Michał Zahuta s26605</a></li>
-                        <li><a target="_blank" href="https://github.com/R1kst3r"><img src="https://github.com/R1kst3r.png">Adam Wróblewski s26604</a></li>
-                    </ul>
-                </li>
-                <li>*Liczymy na solidną 3*</li>
-                <li>
-                    Źródła
-                    <ul>
-                        <li><a target="_blank" href="https://github.com/jdan/98.css">98.css</a></li>
-                        <li><a target="_blank" href="https://github.com/divshot/geo-bootstrap">Zdjęcia</a></li>
-                    </ul>
-                </li>
-            </ul>
-            <img id="webAward" src="<?php site_url() ?>/template/img/fillers/community.gif" alt="Web award">
-        </div>
-        <img id="mchammer" src="<?php site_url() ?>/template/img/fillers/mchammer.gif" alt="mchammer">
-        <img id="wip" src="<?php site_url() ?>/template/img/fillers/wip.gif" alt="Under construction">
+        <?php include_once 'template/footer.php' ?>
     </footer>
 </body>
 </html>
